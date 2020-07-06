be3a0c6400de7f56a7e8c3cc64c72ee4397bf3ed
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-07-06 16:08:35 -0700

    makeHTMLboard2 function works

--- jeopardy.js
+++ jeopardy.js
@@ -28,14 +28,167 @@ async function setUp(height, width) {
     const WIDTH = width
 
     const categories = await getCategories(100);
-    const selectCats = await getSelects(categories);
+    const selectCats = await getSelects(categories, WIDTH);
     const board = await getClues(selectCats, HEIGHT, WIDTH);
+    await makeHtmlBoard2(HEIGHT, WIDTH, board);
 
-    console.log(board);
+    
 
 }
 
 setUp(5, 6);
+
+
+
+
+
+function makeHtmlBoard2(height, width, board) {
+    const HEIGHT = height;
+    const WIDTH = width;
+    const BOARD = board;
+    // - get "htmlBoard" variable from the item in HTML w/ID of "board"
+    const htmlBoard = document.querySelector("#board");
+    // - create table row that will be a place where the user clicks to choose where to drop piece
+    const top = document.createElement("tr");
+    // - gives that row an ID of column-top
+    top.setAttribute("id", "column-top");
+    // - add click listener and runs the handleClick function when that row is clicked
+    // top.addEventListener("click", handleClick);
+    // - loop to iterate over the first row in the table
+    for (let x = 0; x < WIDTH; x++) {
+      // - create a headCell (data cell)
+      const headCell = document.createElement("td");
+      // - give that data cell an ID of unique number
+      headCell.setAttribute("id", x);
+  
+      // - add the data cell to the top row
+      top.append(headCell);
+    }
+    // - add that top row to the html Board
+    htmlBoard.append(top);
+   
+    
+    
+    // - iterate over the set board width and height to make the board where pieces fall into
+    for (let y = 0; y < HEIGHT; y++) {
+      // - create a row
+      const row = document.createElement("tr");
+      // - iterate over each row to add a data cell for the number of width chosen
+      for (let x = 0; x < WIDTH; x++) {
+        // - add data cell where the piece will be in
+        const cell = document.createElement("td");
+        // - add an ID of a unique number for each data cell
+        const front = document.createElement("div");
+        front.setAttribute("data-key",`${y}-${x}`)
+        front.classList.add("card-front");
+        const frontH3 = document.createElement("h3");
+        frontH3.innerText = board[y][x].question;
+        console.log(board);
+        
+        // console.log(board[0][y].question);
+ 
+
+        const back = document.createElement("div");
+        back.classList.add("card-back");
+        const backP = document.createElement("p");
+        backP.innerText = board[y][x].answer;
+
+
+        // const $item = $(
+        //     `<div class='card-front' data-key='${y}-${x}'>
+        //     <h3>${board.question}</h3>
+        //     </div>
+        //     <div class='card-back'>
+        //     <p>${board.answer}</p>
+        //     </div>`
+        //   )
+  
+        // - append those cells to each row
+        front.append(frontH3);
+        back.append(backP)
+        cell.append(front);
+        cell.append(back)
+        row.append(cell);
+      }
+      // - append each row to the htmlBoard
+      htmlBoard.append(row);
+    }
+}
+
+
+
+
+
+
+// function makeHtmlBoard(height, width, board) {
+//     const HEIGHT = height;
+//     const WIDTH = width;
+//     const BOARD = board;
+//     // - get "htmlBoard" variable from the item in HTML w/ID of "board"
+//     const htmlBoard = document.querySelector("#board");
+//     // - create table row that will be a place where the user clicks to choose where to drop piece
+//     const top = document.createElement("tr");
+//     // - gives that row an ID of column-top
+//     top.setAttribute("id", "column-top");
+//     // - add click listener and runs the handleClick function when that row is clicked
+//     // top.addEventListener("click", handleClick);
+//     // - loop to iterate over the first row in the table
+//     for (let x = 0; x < WIDTH; x++) {
+//       // - create a headCell (data cell)
+//       const headCell = document.createElement("td");
+//       // - give that data cell an ID of unique number
+//       headCell.setAttribute("id", x);
+  
+//       // - add the data cell to the top row
+//       top.append(headCell);
+//     }
+//     // - add that top row to the html Board
+//     htmlBoard.append(top);
+  
+//     // - iterate over the set board width and height to make the board where pieces fall into
+//     for (let y = 0; y < HEIGHT; y++) {
+//       // - create a row
+//       const row = document.createElement("tr");
+//       // - iterate over each row to add a data cell for the number of width chosen
+//       for (let x = 0; x < WIDTH; x++) {
+//         // - add data cell where the piece will be in
+//         const cell = document.createElement("td");
+//         // - add an ID of a unique number for each data cell
+
+//         const $item = $(
+//             `<div class='card-front' data-key='${y}-${x}'>
+//             <h3>${board.question}</h3>
+//             </div>
+//             <div class='card-back'>
+//             <p>${board.answer}</p>
+//             </div>`
+//           )
+  
+//         // - append those cells to each row
+//         cell.append($item);
+//         row.append(cell);
+//       }
+//       // - append each row to the htmlBoard
+//       htmlBoard.append(row);
+//     }
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -61,9 +214,10 @@ const board = new Array(HEIGHT);
 
 
 
-async function getSelects(categories) {
+async function getSelects(categories, width) {
     const selectCats = [];
-    for (let i = 0; i < 6; i++) {
+    const WIDTH = width;
+    for (let i = 0; i < WIDTH; i++) {
         let randomNum = Math.floor(Math.random() * 100);
         const res = await axios.get("https://jservice.io/api/clues", {
             params: {

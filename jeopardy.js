c9c80a6993536fa680759f4770e927e99c59bd9e
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-07-07 13:30:01 -0700

    trying to use a boolean to only allow one click a time didnt work.

--- jeopardy.js
+++ jeopardy.js
@@ -60,75 +60,29 @@ setUp(5, 6);
 
 function listening() {
     const cards = document.querySelectorAll(".money-amount");
+    const cardContainer = document.querySelector(".card-container");
+   
+
+
+
+    
+
     cards.forEach((card) => {
         card.addEventListener("click", (e) => {
-            console.log(e.target);
-            
-            
-            let money;
-            let question;
-            let answer;
-            // if (e.target.classList.contains("money-amount")) {
                 money = e.target;
                 question = e.target.parentElement.children[1];
                 answer = e.target.parentElement.lastChild;
-            // }
-            // else if (e.target.dataset.name === "H4") {
-            //     question = e.target.parentElement;
-            //     answer = e.target.parentElement.parentElement.lastChild.firstChild;
-            // } else {
-                // console.log(e.target);
-                
-                // question = e.target;
-                // console.log("hit a card front div");
-                // answer = e.target.parentElement.lastChild.firstChild;
-            // }
 
-            
+                
                 money.classList.add("flip");
                 question.classList.toggle("flip");
                 setTimeout(() => {
+             
                     question.classList.toggle("flip");
                     answer.classList.remove("flip");
-                    console.log("returning from removing flip from answer");
                 }, 2000);
-             
-        
-
-            // if (!money.classList.contains("flip")) {
-            //     money.classList.add("flip");
-            //     console.log("returning from adding flip to money");
-                
-                
-            // } 
-            // if (question.classList.contains("flip")) {
-            //     question.classList.remove("flip");
-            //     console.log("returning from removing flip from question");
-            //     return;
-            // } 
-            // if (answer.classList.contains("flip")) {
-            //     answer.classList.remove("flip");
-            //     console.log("returning from removing flip from answer");
-            // } 
-
         })
     })
-
-
-
-    // for (let i = 1; i <= HEIGHT; i++) {
-
-    //     console.log(htmlBoard.children[i].children);
-
-    //     for (let j = 0; j <= WIDTH; j++) {
-
-    //         console.log(htmlBoard.children[i].children[0]);
-
-    //     }
-
-    // }
-
-
 }
 
 

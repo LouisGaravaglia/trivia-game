4e34588bc84fbc1f687a0827d7ea66c52c28a415
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-07-25 13:51:14 -0700

    removed unnessary comments.

--- jeopardy.js
+++ jeopardy.js
@@ -321,25 +321,22 @@ async function getSelects(categories, width) {
     for (let i = 0; i < width; i++) {
 
         const randomNum = Math.floor(Math.random() * 100);
-
-        //Randomly pick a category from 100 choices from the return value of the API.
         const res = await axios.get("https://jservice.io/api/clues", {
             params: {
                 category: categories[randomNum].id
             }
         });
 
-        //Push that category to selectCats.
         selectCats.push(res.data);
 
     }
 
-    //Loop over the selectCats array to get a hold of the title of each category and push to it's own array.
     for (let i = 0; i < width; i++) {
+
         titles.push(selectCats[i][0].category.title)
+
     }
 
-    //Return selectCats and titles to use in getClues() and makeHtmlBoard().
     return {
         selectCats: selectCats,
         titles: titles
@@ -359,26 +356,28 @@ async function getSelects(categories, width) {
  */
 async function getClues(selectCats, height, width) {
 
-    //board is set to a array with the length of height.
     const board = new Array(height);
 
-    //Loop to create a 2D array with an array with length of width for reach index of the board array.
     for (let i = 0; i < height; i++) {
+
         board[i] = new Array(width);
+
     }
 
-    //Loop to set the value of each index of the 2D array with a category title, question, and answer.
     for (let j = 0; j < width; j++) {
+
         for (let k = 0; k < height; k++) {
+
             board[k][j] = {
                 title: selectCats[j][k].category.title,
                 question: selectCats[j][k].question,
                 answer: selectCats[j][k].answer
             };
+
         }
+
     }
 
-    //Returning the board to be used in makeHtmlBoard().
     return board;
 
 };
@@ -396,58 +395,45 @@ async function getClues(selectCats, height, width) {
  */
 async function makeHtmlBoard(height, width, board, titles) {
 
-    //Get a hold of the table with an ID of "#board".
     const htmlBoard = document.querySelector("#board");
-
-    //Call makeTopRow() to populate the top row of the game board with the category titles.
     const top = makeTopRow(width, titles);
 
-    //Add the top row to the table htmlBoard.
     htmlBoard.append(top);
 
-    //Run a loop to assign a table row element to each index of height.
     for (let y = 0; y < height; y++) {
+
         const row = document.createElement("tr");
 
-        //Run a loop to assign table data cells to each value in the 2D array.
         for (let x = 0; x < width; x++) {
 
-            //Create the data cell and add the class of ".card-box".
             const cell = document.createElement("td");
             cell.classList.add("card-box");
 
-            //Create a div to contain how much money that question is worth and a data-name attribute to keep track.
             const moneyDiv = document.createElement("div");
             moneyDiv.setAttribute("data-name", "MONEY")
             moneyDiv.classList.add("money-amount");
 
-            //Create a paragraph element with its innerText to be incrementing values by $200, starting at $200, from the the top.
             const moneyText = document.createElement("p");
             moneyText.innerText = `$${(y+1)*2}00`;
 
-            //Create a div to contain a unique data-key and a class list of ".flip" to start it out as disply: none.
             const questionDiv = document.createElement("div");
             questionDiv.setAttribute("data-key", `${y}-${x}`)
             questionDiv.classList.add("card-front");
             questionDiv.classList.add("flip");
 
-            //Create a paragraph element to contain the appropriate question from the category.
             const questionText = document.createElement("p");
             questionText.setAttribute("data-name", "P")
             const capitalizedQuestion = _.toUpper(board[y][x].question);
             questionText.innerText = capitalizedQuestion.replace(/(<([^>]+)>)/ig, "");
 
-            //Create a div to hold the answer and a class list of ".flip" to start it out as disply: none.
             const answerDiv = document.createElement("div");
             answerDiv.classList.add("card-back");
             answerDiv.classList.add("flip");
 
-            //Create a paragraph element whose innerText is set to the appropriate answer from the category.
             const answerText = document.createElement("p");
             const capitalizedAnswer = _.toUpper(board[y][x].answer);
             answerText.innerText = capitalizedAnswer.replace(/(<([^>]+)>)/ig, "");
 
-            //Append elements to their parent divs, then append divs to the data cell, then append data cell to the row. (Repeat width x height times)
             moneyDiv.append(moneyText);
             questionDiv.append(questionText);
             answerDiv.append(answerText);
@@ -455,10 +441,11 @@ async function makeHtmlBoard(height, width, board, titles) {
             cell.append(questionDiv);
             cell.append(answerDiv)
             row.append(cell);
+
         }
 
-        //Append the row to the htmlBoard (Repeat height times)
         htmlBoard.append(row);
+
     }
 
 }
@@ -474,32 +461,26 @@ async function makeHtmlBoard(height, width, board, titles) {
  */
 function makeTopRow(width, titles) {
 
-    //Create a table row element to add category titles.
     const topRow = document.createElement("tr");
-
-    //Add attribute of id to "column-top".
     topRow.setAttribute("id", "column-top");
 
-    //Loop of the width of the row to a table data cell.
     for (let x = 0; x < width; x++) {
+
         const titleCell = document.createElement("td");
         titleCell.setAttribute("id", x);
 
-        //Create a div to hold the title paragraph element.
         const titleBox = document.createElement("div");
         titleBox.classList.add("title-box");
 
-        //Create a paragraph elmenet to hold the capitalized title.
         const title = document.createElement("p");
         title.innerText = _.toUpper(titles[x]);
 
-        //Append the title to its parent div, then the div to the topRow.
         titleBox.append(title);
         titleCell.append(titleBox);
         topRow.append(titleCell);
+
     }
 
-    //Return topRow to be used in makeHtmlBoard().
     return topRow;
 
 }
@@ -513,27 +494,17 @@ function makeTopRow(width, titles) {
  */
 function hideLoadingAndShowGame() {
 
-    //Create a variable for the container that holds the "JEOPARDY" text to show when loading API content.
     const loadingContainer = document.querySelector(".loading-container");
-
-    //Create a variable for the input where the user enters their guess.
     const inputContainer = document.querySelector(".input-group");
-
-    //Create a variable for the container that holds the score value.
     const scoreContainer = document.querySelector(".score-container");
-
-    //Create a variable for the container that holds the reset button.
     const resetContainer = document.querySelector(".reset-container");
 
-    //Toggle the flip class on to hide the loading "JEOPARDY" text.
     loadingContainer.classList.toggle("flip");
 
-    //Toggle the flip class off on these rest to reveal them.
     inputContainer.classList.toggle("flip");
     scoreContainer.classList.toggle("flip");
     resetContainer.classList.toggle("flip");
 
-    //Set the score to "000" to start the game off.
     sessionStorage.setItem("score", `000`);
 
 }
@@ -553,28 +524,22 @@ function hideLoadingAndShowGame() {
  */
 function clockTicking(timeLimit, question, answer) {
 
-    //Get a hold of the body.
     const body = document.querySelector("body");
 
-    //Create a container div to contain the clock box.
     const clockContainer = document.createElement("div");
     clockContainer.classList.add("clock-container");
 
-    //Create a box div to contain the h1 clock
     const clockBox = document.createElement("div");
     clockBox.classList.add("clock-box");
 
-    //Create a h1 element to hold the countdown clock.
     const clock = document.createElement("h1");
     clock.classList.add("danger", "clock");
     clock.innerText = `00:${timeLimit}`;
 
-    //Append the clock container and its children to the body.
     clockBox.append(clock);
     clockContainer.append(clockBox);
     body.append(clockContainer);
 
-    //Call timerInterval() to start the clock countdown.
     timerInterval(timeLimit, clock, clockContainer, question, answer);
 
 }
@@ -593,64 +558,54 @@ function clockTicking(timeLimit, question, answer) {
  */
 function timerInterval(timeLimit, clock, clockContainer, question, answer) {
 
-    //Initialize timePassed to be set to 0.
     let timePassed = 0;
     let timeLeft;
 
-    //Initialize the value of "#stop-timer" to false so that the count down continues.
     $("#stop-timer").val(false);
 
-    //Establish and run the timer every second.
     let timer = setInterval(() => {
 
-        //Use timePassed to keep track of how much time has passed.
         timePassed = timePassed += 1;
-
-        //Subtrack timePassed by the designated timeLimit to get timeLeft.
         timeLeft = timeLimit - timePassed;
 
-        //Display timeLeft as the clock's element innerText.
         clock.innerText = `00:0${timeLeft}`;
 
-        //Condtional to see if "#stop-timer" value is set to "true", if so stop the timer and hide the question and show the answer.
         if ($("#stop-timer").val()) {
+
             clearInterval(timer);
+
             clockContainer.classList.add("flip");
             question.classList.toggle("flip");
             answer.classList.remove("flip");
+
         }
 
-        //If the timer has run out of time.
         if (timeLeft === 0) {
 
-            //Display the "TIME'S UP" container, then remove it after a second and half.
             const timesUpContainer = document.querySelector(".times-up-container");
-
-            //Get a hold of the body.
             const body = document.querySelector("body");
-
-            //Create a container to block the user from cliking any DOM elements.
             const noClickContainer = document.createElement("div");
-            noClickContainer.classList.add("no-clicking-container");
 
-            //Append the noClickContainer to the body.
+            noClickContainer.classList.add("no-clicking-container");
             body.append(noClickContainer);
 
-            //Display the "TIME'S UP" container, then remove it after a second and half.
             timesUpContainer.classList.toggle("flip");
 
-            //Remove the "TIME'S UP" alert and the no cliking div after a second and half.
             setTimeout(() => {
+
                 timesUpContainer.classList.toggle("flip");
                 noClickContainer.classList.toggle("flip");
+
             }, 1500);
 
-            //Clear the timer, hide the question, and show the answer.
             clearInterval(timer);
+
             clockContainer.classList.add("flip");
             question.classList.toggle("flip");
             answer.classList.remove("flip");
+
         }
+        
     }, 1000);
 
 }
